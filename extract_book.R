684e1b48ac78e9059d2f59f5a10d6306f840b4ec
miniohn <22100456@handong.ac.kr>
2025-05-23 21:59:09 +0900

    update

--- extract_book.R
+++ extract_book.R
@@ -17,11 +17,14 @@ for (f in temp) {
   assign(var_name, pdf_text(f))
 }
 
+out_dir <- "/Users/haley/Desktop/2025-1/DS1/code/need_preprocessing/Book/"
+
 for (f in temp) {
-  var_name <- tools::file_path_sans_ext(f)     # "book1"
-  content <- get(var_name)                     # book1이라는 변수에서 가져옴
-  txt_name <- paste0(var_name, ".txt")         # "book1.txt"
-  writeLines(content, txt_name)
+  var_name <- tools::file_path_sans_ext(f)         # 예: "book1"
+  content <- get(var_name)                         # 변수에서 내용 불러오기
+  txt_name <- paste0(var_name, ".txt")             # 예: "book1.txt"
+  out_path <- file.path(out_dir, txt_name)         # 전체 경로 만들기
+  writeLines(content, out_path)                    # 해당 경로에 저장
 }
 
 
@@ -30,4 +33,4 @@ doc <- read_docx("./book12.docx")
 xml <- docx_summary(doc)
 body_text <- subset(xml, content_type == "paragraph") # only paragraph
 cat(paste(body_text$text, collapse = "\n"))
-writeLines(body_text$text, "book12.txt")
+writeLines(body_text$text, "/Users/haley/Desktop/2025-1/DS1/code/need_preprocessing/Book/book12.txt")
